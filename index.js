4b6c25e83a6ef24498f9b70e08518012682890b7
mss38 <mss38k@gmail.com>
2022-05-07 12:14:56 +0600

    fetch mongodb collection data from server

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
+
+const app = express();
+
+//MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nvxwy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const stockCollection = client.db("wareHouse").collection("stock");
+
+    app.get("/stock", async (req, res) => {
+      const quary = {};
+      const cursor = stockCollection.find(quary);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Warehouse connected to server. Response available in browser.");
