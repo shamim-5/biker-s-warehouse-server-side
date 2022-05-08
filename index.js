7115bb02698f2e74dcd91d8eb01efba9b5749d93
mss38 <mss38k@gmail.com>
2022-05-08 12:41:19 +0600

    added delete functionality on manage-inventory pages

--- index.js
+++ index.js
@@ -44,8 +44,13 @@ async function run() {
       res.send(inventory);
     });
 
-    //POST
-    
+    //DELETE
+    app.delete("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
